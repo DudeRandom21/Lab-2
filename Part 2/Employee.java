dbf31cd784dd59738c485ef3929e0e62396e3f41
frank <fding036@uottawa.ca>
2017-09-28 20:56:59 +0100

    Version2 Part2, completed

---
+++ Part 2/Employee.java
@@ -0,0 +1,21 @@
+public class Employee{
+
+	//instance variables
+	public String name;
+	public int hours;
+	public double rate;
+	public Address[] address;
+
+
+	public String getName(){
+		return name;
+	}
+	public int getHours(){
+		return hours;
+	}
+	public double getRate(){
+		return rate;
+	}
+
+
+}
